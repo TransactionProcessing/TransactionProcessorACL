9255e7f5324735fdd3481a27c66559bba1633581
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2022-10-22 14:47:36 +0100

    Fix nigtly builds

--- TransactionProcessorACL.IntegrationTests/Shared/SharedSteps.cs
+++ TransactionProcessorACL.IntegrationTests/Shared/SharedSteps.cs
@@ -9,6 +9,7 @@
     using System.Threading;
     using System.Threading.Tasks;
     using Common;
+    using DataTransferObjects;
     using EstateManagement.DataTransferObjects;
     using EstateManagement.DataTransferObjects.Requests;
     using EstateManagement.DataTransferObjects.Responses;
@@ -235,7 +236,7 @@
 
                 // Get current balance
                 MerchantBalanceResponse previousMerchantBalance =
-                    await this.TestingContext.DockerHelper.EstateClient.GetMerchantBalance(token, estateDetails.EstateId, merchantId, CancellationToken.None);
+                    await this.TestingContext.DockerHelper.TransactionProcessorClient.GetMerchantBalance(token, estateDetails.EstateId, merchantId, CancellationToken.None);
 
                 MakeMerchantDepositRequest makeMerchantDepositRequest = new MakeMerchantDepositRequest
                                                                         {
@@ -264,7 +265,7 @@
                 await Retry.For(async () =>
                                 {
                                     MerchantBalanceResponse currentMerchantBalance =
-                                        await this.TestingContext.DockerHelper.EstateClient.GetMerchantBalance(token,
+                                        await this.TestingContext.DockerHelper.TransactionProcessorClient.GetMerchantBalance(token,
                                                                                                                estateDetails.EstateId,
                                                                                                                merchantId,
                                                                                                                CancellationToken.None);
