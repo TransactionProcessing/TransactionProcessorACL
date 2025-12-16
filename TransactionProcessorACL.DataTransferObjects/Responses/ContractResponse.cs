35e252be86b9180b335a01c24c48d176df81168b
StuartFerguson <stuart_ferguson1development@outlook.com>
2025-12-16 05:29:03 +0000

    :|

--- TransactionProcessorACL.DataTransferObjects/Responses/ContractResponse.cs
+++ TransactionProcessorACL.DataTransferObjects/Responses/ContractResponse.cs
@@ -32,33 +32,35 @@ namespace TransactionProcessorACL.DataTransferObjects.Responses {
 
     public class ContractProduct
     {
+        [JsonProperty("display_text")]
         public string DisplayText { get; set; }
-
+        [JsonProperty("name")]
         public string Name { get; set; }
-
+        [JsonProperty("product_id")]
         public Guid ProductId { get; set; }
-
+        [JsonProperty("product_reporting_id")]
         public int ProductReportingId { get; set; }
-
+        [JsonProperty("transaction_fees")]
         public List<ContractProductTransactionFee> TransactionFees { get; set; }
-
+        [JsonProperty("value")]
         public Decimal? Value { get; set; }
-
+        [JsonProperty("product_type")]
         public ProductType ProductType { get; set; }
     }
 
     public class ContractProductTransactionFee
     {
+        [JsonProperty("calculation_type")]
         public CalculationType CalculationType { get; set; }
-
+        [JsonProperty("fee_type")]
         public FeeType FeeType { get; set; }
-
+        [JsonProperty("description")]
         public string Description { get; set; }
-
+        [JsonProperty("transaction_fee_id")]
         public Guid TransactionFeeId { get; set; }
-
+        [JsonProperty("transaction_fee_reporting_id")]
         public int TransactionFeeReportingId { get; set; }
-
+        [JsonProperty("value")]
         public Decimal Value { get; set; }
     }
 
