403a6c7366cf51c2898e41b7a82fdc8f3826b37a
StuartFerguson <stuart_ferguson1development@outlook.com>
2026-03-28 17:15:45 +0000

    Make Sentry options configurable via app settings
    Allow CaptureBlockingCalls and IncludeActivityData Sentry options to be set from configuration using ConfigurationReader, defaulting to false if not specified. This enables more flexible control of Sentry behavior without code changes.

--- TransactionProcessorACL/Program.cs
+++ TransactionProcessorACL/Program.cs
@@ -95,7 +95,8 @@ namespace TransactionProcessorACL
                                                                      o.Dsn = builtConfig["SentryConfiguration:Dsn"];
                                                                      o.SendDefaultPii = true;
                                                                      o.MaxRequestBodySize = RequestSize.Always;
-                                                                     o.CaptureBlockingCalls = true;
+                                                                     o.CaptureBlockingCalls = ConfigurationReader.GetValueOrDefault("SentryConfiguration", "CaptureBlockingCalls", false);
+                                                                     o.IncludeActivityData = ConfigurationReader.GetValueOrDefault("SentryConfiguration", "IncludeActivityData", false);
                                                                      o.Release = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "unknown";
                                                                  });
                                                              }
