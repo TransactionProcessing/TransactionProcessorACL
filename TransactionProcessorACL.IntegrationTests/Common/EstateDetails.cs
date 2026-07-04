3c171bfc61264e2a4511207a1fb012b3bcb7e722
StuartFerguson <stuart_ferguson1development@outlook.com>
2026-07-04 15:04:11 +0100

    Add integration tests for Estate Reporting API
    - Add scenarios and step definitions for merchant daily performance and transaction mix summary endpoints
    - Update DockerHelper and test setup for EstateReporting container support
    - Extend EstateDetails1 to track reporting IDs and responses
    - Add Reporting.feature and generated .cs for new tests
    - Add ReportingDtos.cs for reporting API response deserialization
    - Update Shared.IntegrationTesting and ClientProxyBase package versions
    - Refactor and clean up code to support new reporting test flows

--- TransactionProcessorACL.IntegrationTests/Common/EstateDetails.cs
+++ TransactionProcessorACL.IntegrationTests/Common/EstateDetails.cs
@@ -1,19 +1,30 @@
-﻿using TransactionProcessor.IntegrationTesting.Helpers;
+using TransactionProcessor.IntegrationTesting.Helpers;
+using TransactionProcessorACL.DataTransferObjects.Responses;
+using TransactionProcessorACL.IntegrationTests.Shared;
 
 namespace TransactionProcessor.IntegrationTests.Common
 {
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    
-    public class EstateDetails1{
+
+    public class EstateDetails1
+    {
         public readonly EstateDetails EstateDetails;
 
         private readonly Dictionary<Guid, Dictionary<String, String>> MerchantUsersTokens;
+        private readonly Dictionary<Guid, Int32> MerchantReportingIds;
+        private readonly Dictionary<Guid, MerchantDailyPerformanceSummaryResponse> MerchantDailyPerformanceSummaryResponses;
+        private readonly Dictionary<Guid, MerchantTransactionMixSummaryResponse> MerchantTransactionMixSummaryResponses;
         private Dictionary<String, Dictionary<String, String>> VoucherRedemptionUsersTokens;
-        public EstateDetails1(EstateDetails estateDetails){
+
+        public EstateDetails1(EstateDetails estateDetails)
+        {
             this.EstateDetails = estateDetails;
             this.MerchantUsersTokens = new Dictionary<Guid, Dictionary<String, String>>();
+            this.MerchantReportingIds = new Dictionary<Guid, Int32>();
+            this.MerchantDailyPerformanceSummaryResponses = new Dictionary<Guid, MerchantDailyPerformanceSummaryResponse>();
+            this.MerchantTransactionMixSummaryResponses = new Dictionary<Guid, MerchantTransactionMixSummaryResponse>();
             this.VoucherRedemptionUsersTokens = new Dictionary<String, Dictionary<String, String>>();
         }
 
@@ -34,6 +45,45 @@ namespace TransactionProcessor.IntegrationTests.Common
                 Dictionary<String, String> merchantUsersList = new Dictionary<String, String>();
                 merchantUsersList.Add(userName, token);
                 this.MerchantUsersTokens.Add(merchantId, merchantUsersList);
+            }
+        }
+
+        public void AddMerchantReportingId(Guid merchantId,
+                                           Int32 merchantReportingId)
+        {
+            if (this.MerchantReportingIds.ContainsKey(merchantId))
+            {
+                this.MerchantReportingIds[merchantId] = merchantReportingId;
+            }
+            else
+            {
+                this.MerchantReportingIds.Add(merchantId, merchantReportingId);
+            }
+        }
+
+        public void AddMerchantDailyPerformanceSummaryResponse(Guid merchantId,
+                                                               MerchantDailyPerformanceSummaryResponse response)
+        {
+            if (this.MerchantDailyPerformanceSummaryResponses.ContainsKey(merchantId))
+            {
+                this.MerchantDailyPerformanceSummaryResponses[merchantId] = response;
+            }
+            else
+            {
+                this.MerchantDailyPerformanceSummaryResponses.Add(merchantId, response);
+            }
+        }
+
+        public void AddMerchantTransactionMixSummaryResponse(Guid merchantId,
+                                                             MerchantTransactionMixSummaryResponse response)
+        {
+            if (this.MerchantTransactionMixSummaryResponses.ContainsKey(merchantId))
+            {
+                this.MerchantTransactionMixSummaryResponses[merchantId] = response;
+            }
+            else
+            {
+                this.MerchantTransactionMixSummaryResponses.Add(merchantId, response);
             }
         }
 
@@ -69,6 +119,21 @@ namespace TransactionProcessor.IntegrationTests.Common
             return string.Empty;
         }
 
+        public Int32 GetMerchantReportingId(Guid merchantId)
+        {
+            return this.MerchantReportingIds.Single(x => x.Key == merchantId).Value;
+        }
+
+        public MerchantDailyPerformanceSummaryResponse GetMerchantDailyPerformanceSummaryResponse(Guid merchantId)
+        {
+            return this.MerchantDailyPerformanceSummaryResponses.SingleOrDefault(x => x.Key == merchantId).Value;
+        }
+
+        public MerchantTransactionMixSummaryResponse GetMerchantTransactionMixSummaryResponse(Guid merchantId)
+        {
+            return this.MerchantTransactionMixSummaryResponses.SingleOrDefault(x => x.Key == merchantId).Value;
+        }
+
         public String GetVoucherRedemptionUserToken(String operatorName)
         {
             KeyValuePair<String, Dictionary<String, String>> x = this.VoucherRedemptionUsersTokens.SingleOrDefault(x => x.Key == operatorName);
@@ -81,294 +146,4 @@ namespace TransactionProcessor.IntegrationTests.Common
             return string.Empty;
         }
     }
-
-    /*
-    public class EstateDetails1
-    {
-        #region Fields
-
-        /// <summary>
-        /// The contracts
-        /// </summary>
-        private readonly List<Contract> Contracts;
-
-        private readonly Dictionary<String, Guid> Merchants;
-
-        private readonly Dictionary<String, Dictionary<String, String>> MerchantUsers;
-
-        private readonly Dictionary<String, Dictionary<String, String>> VoucherRedemptionUsers;
-
-        private readonly Dictionary<String, Dictionary<String, String>> MerchantUsersTokens;
-
-        private Dictionary<String, Dictionary<String, String>> VoucherRedemptionUsersTokens;
-
-        private readonly Dictionary<String, Guid> Operators;
-
-        #endregion
-
-        #region Constructors
-
-        private EstateDetails(Guid estateId,
-                              String estateName)
-        {
-            this.EstateId = estateId;
-            this.EstateName = estateName;
-            this.Merchants = new Dictionary<String, Guid>();
-            this.Operators = new Dictionary<String, Guid>();
-            this.MerchantUsers = new Dictionary<String, Dictionary<String, String>>();
-            this.MerchantUsersTokens = new Dictionary<String, Dictionary<String, String>>();
-            this.VoucherRedemptionUsers = new Dictionary<String, Dictionary<String, String>>();
-            this.VoucherRedemptionUsersTokens = new Dictionary<String, Dictionary<String, String>>();
-            this.TransactionResponses = new Dictionary<(Guid merchantId, String transactionNumber, String transactionType), String>();
-            this.ReconciliationResponses = new Dictionary<Guid, String>();
-            this.Contracts = new List<Contract>();
-        }
-
-        #endregion
-
-        #region Properties
-
-        public String AccessToken { get; private set; }
-
-        public Guid EstateId { get; }
-
-        public String EstateName { get; }
-
-        public String EstatePassword { get; private set; }
-
-        public String EstateUser { get; private set; }
-
-        private Dictionary<(Guid merchantId, String transactionNumber, String transactionType), String> TransactionResponses { get; }
-
-        private Dictionary<Guid, String> ReconciliationResponses { get; }
-
-        #endregion
-
-        #region Methods
-
-        /// <summary>
-        /// Adds the contract.
-        /// </summary>
-        /// <param name="contractId">The contract identifier.</param>
-        /// <param name="contractName">Name of the contract.</param>
-        /// <param name="operatorId">The operator identifier.</param>
-        public void AddContract(Guid contractId,
-                                String contractName,
-                                Guid operatorId)
-        {
-            this.Contracts.Add(new Contract
-                               {
-                                   ContractId = contractId,
-                                   Description = contractName,
-                                   OperatorId = operatorId,
-                               });
-        }
-
-        public void AddMerchant(Guid merchantId,
-                                String merchantName)
-        {
-            this.Merchants.Add(merchantName, merchantId);
-        }
-
-        public void AddMerchantUser(String merchantName,
-                                    String userName,
-                                    String password)
-        {
-            if (this.MerchantUsers.ContainsKey(merchantName))
-            {
-                Dictionary<String, String> merchantUsersList = this.MerchantUsers[merchantName];
-                if (merchantUsersList.ContainsKey(userName) == false)
-                {
-                    merchantUsersList.Add(userName, password);
-                }
-            }
-            else
-            {
-                Dictionary<String, String> merchantUsersList = new Dictionary<String, String>();
-                merchantUsersList.Add(userName, password);
-                this.MerchantUsers.Add(merchantName, merchantUsersList);
-            }
-        }
-
-        public void AddVoucherRedemptionUser(String operatorName,
-                                             String userName,
-                                             String password)
-        {
-            if (this.VoucherRedemptionUsers.ContainsKey(operatorName))
-            {
-                Dictionary<String, String> voucherRedemptionUsersList = this.VoucherRedemptionUsers[operatorName];
-                if (voucherRedemptionUsersList.ContainsKey(userName) == false)
-                {
-                    voucherRedemptionUsersList.Add(userName, password);
-                }
-            }
-            else
-            {
-                Dictionary<String, String> voucherRedemptionUsersList = new Dictionary<String, String>();
-                voucherRedemptionUsersList.Add(userName, password);
-                this.VoucherRedemptionUsers.Add(operatorName, voucherRedemptionUsersList);
-            }
-        }
-
-        public void AddMerchantUserToken(String merchantName,
-                                         String userName,
-                                         String token)
-        {
-            if (this.MerchantUsersTokens.ContainsKey(merchantName))
-            {
-                Dictionary<String, String> merchantUsersList = this.MerchantUsersTokens[merchantName];
-                if (merchantUsersList.ContainsKey(userName) == false)
-                {
-                    merchantUsersList.Add(userName, token);
-                }
-            }
-            else
-            {
-                Dictionary<String, String> merchantUsersList = new Dictionary<String, String>();
-                merchantUsersList.Add(userName, token);
-                this.MerchantUsersTokens.Add(merchantName, merchantUsersList);
-            }
-        }
-
-        public void AddVoucherRedemptionUserToken(String operatorName,
-                                                  String userName,
-                                                  String token)
-        {
-            if (this.VoucherRedemptionUsersTokens.ContainsKey(operatorName))
-            {
-                Dictionary<String, String> merchantUsersList = this.VoucherRedemptionUsersTokens[operatorName];
-                if (merchantUsersList.ContainsKey(userName) == false)
-                {
-                    merchantUsersList.Add(userName, token);
-                }
-            }
-            else
-            {
-                Dictionary<String, String> merchantUsersList = new Dictionary<String, String>();
-                merchantUsersList.Add(userName, token);
-                this.VoucherRedemptionUsersTokens.Add(operatorName, merchantUsersList);
-            }
-        }
-
-        public void AddOperator(Guid operatorId,
-                                String operatorName)
-        {
-            this.Operators.Add(operatorName, operatorId);
-        }
-
-        public void AddTransactionResponse(Guid merchantId,
-                                           String transactionNumber,
-                                           String transactionType,
-                                           String transactionResponse)
-        {
-            this.TransactionResponses.Add((merchantId, transactionNumber, transactionType), transactionResponse);
-        }
-
-        public void AddReconciliationResponse(Guid merchantId,
-                                           String transactionResponse)
-        {
-            this.ReconciliationResponses.Add(merchantId, transactionResponse);
-        }
-
-        public static EstateDetails Create(Guid estateId,
-                                           String estateName)
-        {
-            return new EstateDetails(estateId, estateName);
-        }
-
-        public String GetVoucherRedemptionUserToken(String operatorName)
-        {
-            KeyValuePair<String, Dictionary<String, String>> x = this.VoucherRedemptionUsersTokens.SingleOrDefault(x => x.Key == operatorName);
-
-            if (x.Value != null)
-            {
-                return x.Value.First().Value;
-            }
-
-            return string.Empty;
-        }
-
-        /// <summary>
-        /// Gets the contract.
-        /// </summary>
-        /// <param name="contractName">Name of the contract.</param>
-        /// <returns></returns>
-        public Contract GetContract(String contractName)
-        {
-            if (this.Contracts.Any() == false)
-            {
-                return null;
-            }
-
-            return this.Contracts.Single(c => c.Description == contractName);
-        }
-
-        /// <summary>
-        /// Gets the contract.
-        /// </summary>
-        /// <param name="contractId">The contract identifier.</param>
-        /// <returns></returns>
-        public Contract GetContract(Guid contractId)
-        {
-            return this.Contracts.Single(c => c.ContractId == contractId);
-        }
-
-        public Guid GetMerchantId(String merchantName)
-        {
-            return this.Merchants.Single(m => m.Key == merchantName).Value;
-        }
-
-        public String GetMerchantUserToken(String merchantName)
-        {
-            KeyValuePair<String, Dictionary<String, String>> x = this.MerchantUsersTokens.SingleOrDefault(x => x.Key == merchantName);
-
-            if (x.Value != null)
-            {
-                return x.Value.First().Value;
-            }
-
-            return string.Empty;
-        }
-
-        public Guid GetOperatorId(String operatorName)
-        {
-            return this.Operators.Single(o => o.Key == operatorName).Value;
-        }
-
-        public String GetReconciliationResponse(Guid merchantId)
-        {
-            var reconciliationResponse =
-                this.ReconciliationResponses
-                    .Where(t => t.Key == merchantId)
-                    .SingleOrDefault();
-
-            return reconciliationResponse.Value;
-        }
-
-        public String GetTransactionResponse(Guid merchantId,
-                                             String transactionNumber,
-                                             String transactionType)
-        {
-            KeyValuePair<(Guid merchantId, String transactionNumber, String transactionType), String> transactionResponse =
-                this.TransactionResponses
-                    .Where(t => t.Key.merchantId == merchantId && t.Key.transactionNumber == transactionNumber && t.Key.transactionType == transactionType)
-                    .SingleOrDefault();
-
-            return transactionResponse.Value;
-        }
-
-        public void SetEstateUser(String userName,
-                                  String password)
-        {
-            this.EstateUser = userName;
-            this.EstatePassword = password;
-        }
-
-        public void SetEstateUserToken(String accessToken)
-        {
-            this.AccessToken = accessToken;
-        }
-        
-        #endregion
-    }*/
 }
---
+++ TransactionProcessorACL.IntegrationTests/Reporting/Reporting.feature
@@ -0,0 +1,130 @@
+@base @shared @reporting
+Feature: Reporting
+
+Background:
+
+	Given the following security roles exist
+	| Role Name |
+	| Merchant   |
+
+	Given I create the following api scopes
+	| Name                 | DisplayName                       | Description                            |
+	| transactionProcessor | Transaction Processor REST  Scope | A scope for Transaction Processor REST |
+	| transactionProcessorACL | Transaction Processor ACL REST  Scope | A scope for Transaction Processor ACL REST |
+	| estateReporting      | Estate Reporting REST Scope      | Scope for Estate Reporting REST      |
+	
+	Given the following api resources exist
+	| Name            | DisplayName                    | Secret  | Scopes                  | UserClaims                 |
+	| transactionProcessor    | Transaction Processor REST     | Secret1 | transactionProcessor    | merchantId, estateId, role |
+	| transactionProcessorACL | Transaction Processor ACL REST | Secret1 | transactionProcessorACL | merchantId, estateId, role |
+	| estateReporting      | Estate Reporting REST      | Secret1 | estateReporting      | merchantId,estateId,role |
+
+	Given the following clients exist
+	| ClientId       | ClientName      | Secret  | Scopes                                                                   | GrantTypes  |
+	| serviceClient  | Service Client  | Secret1 | transactionProcessor,transactionProcessorACL, estateReporting | client_credentials |
+	| merchantClient | Merchant Client | Secret1 | transactionProcessorACL                                                         | password           |
+
+	Given I have a token to access the estate management and transaction processor acl resources
+	| ClientId      | 
+	| serviceClient | 
+
+	Given I have created the following estates
+	| EstateName    |
+	| Test Estate 1 |
+	| Test Estate 2 |
+
+	Given I have created the following operators
+	| EstateName    | OperatorName | RequireCustomMerchantNumber | RequireCustomTerminalNumber |
+	| Test Estate 1 | Safaricom    | True                        | True                        |
+	| Test Estate 1 | Voucher      | True                        | True                        |
+	| Test Estate 2 | Safaricom    | True                        | True                        |
+	| Test Estate 2 | Voucher      | True                        | True                        |
+
+	And I have assigned the following operators to the estates
+	| EstateName    | OperatorName    | 
+	| Test Estate 1 | Safaricom |
+	| Test Estate 1 | Voucher |
+	| Test Estate 2 | Safaricom |
+	| Test Estate 2 | Voucher |
+
+	Given I create a contract with the following values
+	| EstateName    | OperatorName    | ContractDescription |
+	| Test Estate 1 | Safaricom | Safaricom Contract |
+	| Test Estate 1 | Voucher      | Hospital 1 Contract |
+	| Test Estate 2 | Safaricom | Safaricom Contract |
+	| Test Estate 2 | Voucher      | Hospital 1 Contract |
+
+	When I create the following Products
+	| EstateName    | OperatorName | ContractDescription | ProductName    | DisplayText | Value | ProductType |
+	| Test Estate 1 | Safaricom    | Safaricom Contract  | Variable Topup | Custom      |       | MobileTopup |
+	| Test Estate 1 | Voucher      | Hospital 1 Contract | 10 KES         | 10 KES      |       | Voucher     |
+	| Test Estate 2 | Safaricom    | Safaricom Contract  | Variable Topup | Custom      |       | MobileTopup |
+	| Test Estate 2 | Voucher      | Hospital 1 Contract | 10 KES         | 10 KES      |       | Voucher     |
+
+	When I add the following Transaction Fees
+	| EstateName    | OperatorName | ContractDescription | ProductName    | CalculationType | FeeDescription      | Value |
+	| Test Estate 1 | Safaricom    | Safaricom Contract  | Variable Topup | Fixed           | Merchant Commission | 2.50  |
+	| Test Estate 2 | Safaricom    | Safaricom Contract  | Variable Topup | Percentage      | Merchant Commission | 0.85  |
+
+	Given I create the following merchants
+	| MerchantName    | AddressLine1   | Town     | Region      | PostalCode |Country        | ContactName    | EmailAddress                 | EstateName    |
+	| Test Merchant 1 | Address Line 1 | TestTown | Test Region | TE57 1NG   |United Kingdom | Test Contact 1 | testcontact1@merchant1.co.uk | Test Estate 1 |
+	| Test Merchant 2 | Address Line 1 | TestTown | Test Region | TE57 2NG   |United Kingdom | Test Contact 2 | testcontact2@merchant2.co.uk | Test Estate 1 |
+	| Test Merchant 3 | Address Line 1 | TestTown | Test Region | TE57 3NG   |United Kingdom | Test Contact 3 | testcontact3@merchant2.co.uk | Test Estate 2 |
+
+	Given I have assigned the following  operator to the merchants
+	| OperatorName | MerchantName    | MerchantNumber | TerminalNumber | EstateName    |
+	| Safaricom    | Test Merchant 1 | 00000001       | 10000001       | Test Estate 1 |
+	| Voucher      | Test Merchant 1 | 00000001       | 10000001       | Test Estate 1 |
+	| Safaricom    | Test Merchant 2 | 00000002       | 10000002       | Test Estate 1 |
+	| Voucher      | Test Merchant 2 | 00000002       | 10000002       | Test Estate 1 |
+	| Safaricom    | Test Merchant 3 | 00000003       | 10000003       | Test Estate 2 |
+	| Voucher      | Test Merchant 3 | 00000003       | 10000003       | Test Estate 2 |
+
+	Given I have assigned the following devices to the merchants
+	| DeviceIdentifier | MerchantName    | EstateName    |
+	| 123456780        | Test Merchant 1 | Test Estate 1 |
+	| 123456781        | Test Merchant 2 | Test Estate 1 |
+	| 123456782        | Test Merchant 3 | Test Estate 2 |
+
+	When I add the following contracts to the following merchants
+	| EstateName    | MerchantName    | ContractDescription       |
+	| Test Estate 1 | Test Merchant 1 | Safaricom Contract        |
+	| Test Estate 1 | Test Merchant 1 | Hospital 1 Contract       |
+	| Test Estate 1 | Test Merchant 2 | Safaricom Contract        |
+	| Test Estate 1 | Test Merchant 2 | Hospital 1 Contract       |
+	| Test Estate 2 | Test Merchant 3 | Safaricom Contract        |
+	| Test Estate 2 | Test Merchant 3 | Hospital 1 Contract       |
+
+	Given I make the following manual merchant deposits 
+	| Reference | Amount  | DateTime | MerchantName    | EstateName    |
+	| Deposit1  | 210.00 | Today    | Test Merchant 1 | Test Estate 1 |
+	| Deposit1  | 110.00 | Today    | Test Merchant 2 | Test Estate 1 |
+	| Deposit1  | 110.00 | Today    | Test Merchant 3 | Test Estate 2 |
+
+	Given I have created the following security users
+	| EmailAddress                  | Password | GivenName    | FamilyName | EstateName    | MerchantName    |
+	| merchantuser@testmerchant1.co.uk | 123456   | TestMerchant | User1      | Test Estate 1 | Test Merchant 1 |
+	| merchantuser@testmerchant2.co.uk | 123456   | TestMerchant | User2      | Test Estate 1 | Test Merchant 2 |
+	| merchantuser@testmerchant3.co.uk | 123456   | TestMerchant | User3      | Test Estate 2 | Test Merchant 3 |
+
+@PRTest
+Scenario: Merchant daily performance summary
+	Given I am logged in as "merchantuser@testmerchant1.co.uk" with password "123456" for Merchant "Test Merchant 1" for Estate "Test Estate 1" with client "merchantClient"
+	When I perform the following transactions
+	| DateTime | TransactionNumber | TransactionType | MerchantName    | DeviceIdentifier | EstateName    | OperatorName | TransactionAmount | CustomerAccountNumber | CustomerEmailAddress | ContractDescription | ProductName    | RecipientEmail | RecipientMobile |
+	| Today    | 8                 | Sale            | Test Merchant 1 | 123456780        | Test Estate 1 | Safaricom    | 50.00             | 123456789             |                      | Safaricom Contract  | Variable Topup |                |                 |
+
+	When I get the merchant daily performance summary for Merchant "Test Merchant 1" for Estate "Test Estate 1"
+	Then the merchant daily performance summary response should contain at least one metric and the sale amount 50.00
+
+@PRTest
+@ignore
+Scenario: Merchant transaction mix summary
+	Given I am logged in as "merchantuser@testmerchant1.co.uk" with password "123456" for Merchant "Test Merchant 1" for Estate "Test Estate 1" with client "merchantClient"
+	When I perform the following transactions
+	| DateTime | TransactionNumber | TransactionType | MerchantName    | DeviceIdentifier | EstateName    | OperatorName | TransactionAmount | CustomerAccountNumber | CustomerEmailAddress | ContractDescription | ProductName    | RecipientEmail | RecipientMobile |
+	| Today    | 9                 | Sale            | Test Merchant 1 | 123456780        | Test Estate 1 | Safaricom    | 25.00             | 123456789             |                      | Safaricom Contract  | Variable Topup |                |                 |
+
+	When I get the merchant transaction mix summary for Merchant "Test Merchant 1" for Estate "Test Estate 1"
+	Then the merchant transaction mix summary response should contain at least one item and the sale amount 25.00
